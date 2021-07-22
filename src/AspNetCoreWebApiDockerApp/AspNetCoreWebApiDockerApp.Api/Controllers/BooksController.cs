86a547ebfdd608f7a83daf2c895532976664f19e
Tiago Pala <tiago_pala@outlook.com>
2021-07-21 23:58:15 -0300

    add swagger, add basic crud controller

---
+++ src/AspNetCoreWebApiDockerApp/AspNetCoreWebApiDockerApp.Api/Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿using AspNetCoreWebApiDockerApp.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AspNetCoreWebApiDockerApp.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult Get(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Save([FromBody] Book book)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult Update(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult Delete(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
--- src/AspNetCoreWebApiDockerApp/AspNetCoreWebApiDockerApp.Api/Startup.cs
+++ src/AspNetCoreWebApiDockerApp/AspNetCoreWebApiDockerApp.Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,11 @@ namespace AspNetCoreWebApiDockerApp.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddRouting(options => options.LowercaseUrls = true);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Concepts.Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +41,13 @@ namespace AspNetCoreWebApiDockerApp.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Concepts.Api v1");
+            });
 
             app.UseHttpsRedirection();
 
